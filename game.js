463072429e923ea9420bdd61d8f73cf2499da353
Martin Ananiev <gh05tr1d3r@NotMartins-NotMacBook-VeryPro.local>
2021-08-16 19:42:27 +0300

    First Commit, halfway done

---
+++ game.js
@@ -0,0 +1,62 @@
+// Creating variables
+var socket = io();
+var snekX = [], snekY = [];
+var fruitcol = ["orange","red","yellow","green"]
+var snekcol = [], user = 0;
+var dir = 0; // 0 - up, 1 - right, 2- down, 3- left;
+var hrana ={
+    x:0,
+    y:0,
+}
+for (i = 0;i < 1000;i++){
+    snekcol[i] = Math.floor(Math.random()*16777215).toString(16);
+}
+socket.on("hrana" ,function(x,y){
+    hrana.x=x;
+    hrana.y=y;
+
+});
+socket.on("myrdane",function(id,x,y){
+    snekX[id] = x;
+    snekY[id] = y;
+})
+socket.on("id", function(id,x,y){
+    id = user;
+    snekX[id] = x;
+    snekY[id] = y;    
+});
+function update() {
+    if(isKeyPressed[37]){
+        dir = 3;
+    }
+    if(isKeyPressed[38]){
+        dir = 0;
+    }
+    if(isKeyPressed[39]){
+        dir = 1;
+    }
+    if(isKeyPressed[40]){
+        dir = 2;
+    }
+    switch(dir){
+        case 0:
+            snekY[user]--;
+            break;
+        case 1:
+            snekX[user]++;
+            break;
+        case 2:
+            snekY[user]++;
+            break;
+        case 3:
+            snekX[user]--;
+            break;
+    }
+}
+function draw() {
+}
+
+function keyup(key) {
+}
+function mouseup() {
+}

dfd7351c48d651aa9f7ec3bd0b20b5add536f20c
Martin Ananiev <gh05tr1d3r@NotMartins-NotMacBook-VeryPro.local>
2021-08-18 08:36:13 +0300

    Made little to no progress with group

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ var socket = io();
 var snekX = [], snekY = [];
 var fruitcol = ["orange","red","yellow","green"]
 var snekcol = [], user = 0;
+var timer = 0;
 var dir = 0; // 0 - up, 1 - right, 2- down, 3- left;
 var hrana ={
     x:0,
@@ -38,22 +39,32 @@ function update() {
     if(isKeyPressed[40]){
         dir = 2;
     }
+    if(timer>=20){
     switch(dir){
         case 0:
             snekY[user]--;
+            timer = 0
             break;
         case 1:
             snekX[user]++;
+            timer = 0
             break;
         case 2:
             snekY[user]++;
+            timer = 0
             break;
         case 3:
             snekX[user]--;
+            timer = 0
             break;
-    }
+       }
+    }   
+    ++timer;
 } 
+
+
 function draw() {
+    context.fillRect(snekX[user],snekY[user],30,30)
 }
 
 function keyup(key) {
